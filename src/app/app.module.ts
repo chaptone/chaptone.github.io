eac8cf24514398c129ae066bd706a58af0ac3ed9
Rakkan Jintasatien <rakkan.j@ku.th>
2018-01-29 18:59:14 +0700

    parallax

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ParallaxComponent } from './parallax/parallax.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParallaxComponent
   ],
   imports: [
     BrowserModule
